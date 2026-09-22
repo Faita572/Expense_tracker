ae88feb6bccd1f8f6e07f74cd8d02c85f73509f3
Faith Ngwata <faithngwata059@gmail.com>
2026-09-22 21:42:15 +0530

    Updated the index.py file

--- api/index.py
+++ api/index.py
@@ -1,13 +1,13 @@
 import sys
 import os
 
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from app import app, init_db 
 
 try:
     init_db()
 except Exception as e:
-    print(f"Error initializing database: {e}")  
+    print(f"DB Init Note: {e}")  
 
 app = app
